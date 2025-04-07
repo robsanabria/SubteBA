b3dcfb6e8ad362f3e34c7570f44c4a45b6eebce8
Rob! <sanabria.er@gmail.com>
2025-04-07 17:15:57 -0300

    fix: prepare for Vercel deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
     root: true,
-    extends: ['next/core-web-vitals'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended'
+    ],
     rules: {
         // Disable strict TypeScript rules that might block deployment
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         // Disable rules that might cause issues with Next.js API routes

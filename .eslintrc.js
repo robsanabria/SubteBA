b3f48b1305bc45837e839d56c09890761626006f
Rob! <sanabria.er@gmail.com>
2025-04-07 17:02:49 -0300

    Chore: disable strict TS ESLint rules for deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     root: true,
     extends: ['next', 'next/core-web-vitals'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-    },
+      rules: {
+          "@typescript-eslint/no-unused-vars": "off",
+          "@typescript-eslint/no-explicit-any": "off"
+      }
   };
